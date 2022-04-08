186075192b276152b153dbbead7226bb65493614
Gauthier Baras <101516806+Gauthier5021@users.noreply.github.com>
2022-04-08 16:54:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import math
 import matplotlib.pyplot as plt
 
-#x = [1, 1, 6, 6, 1]
-#y = [1, 6, 1, 1, 1]
+#x = [1, 1, 2, 4, 1]
+#y = [1, 15, 6, 1, 1]
 
-x = [1, 1, 2, 4, 1]
-y = [1, 15, 6, 1, 1]
+x = [1, 2, 1, 6, 5, 7, 1]
+y = [1, 4, 6, 6, 4, 1, 1]
+
 plt.plot(x, y)
-plt.ylabel('Test')
+plt.ylabel('Forme')
 plt.show()
